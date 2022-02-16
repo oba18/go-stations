61351931728ede775af387d14b4ae93f588fddf9
oba18 <m.oba.0426@gmail.com>
2022-02-16 23:05:21 +0900

    station7

--- model/todo.go
+++ model/todo.go
@@ -16,8 +16,8 @@ type (
 
 	// A CreateTODORequest expresses ...
 	CreateTODORequest struct {
-		Subject     string
-		Description string
+		Subject     string `json:"subject"`
+		Description string `json:"description"`
 	}
 	// A CreateTODOResponse expresses ...
 	CreateTODOResponse struct {
