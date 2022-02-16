515d64a35032b7bdfa883c1092e771a7da3d720d
oba18 <m.oba.0426@gmail.com>
2022-02-16 23:02:02 +0900

    station7

--- model/todo.go
+++ model/todo.go
@@ -15,9 +15,18 @@ type (
 	}
 
 	// A CreateTODORequest expresses ...
-	CreateTODORequest struct{}
+	CreateTODORequest struct {
+		Subject     string
+		Description string
+	}
 	// A CreateTODOResponse expresses ...
-	CreateTODOResponse struct{}
+	CreateTODOResponse struct {
+		ID          int       `json:"id"`
+		Subject     string    `json:"subject"`
+		Description string    `json:"description"`
+		CreatedAt   time.Time `json:"created_at"`
+		UpdatedAt   time.Time `json:"updated_at"`
+	}
 
 	// A ReadTODORequest expresses ...
 	ReadTODORequest struct{}
